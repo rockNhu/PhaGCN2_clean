9a457da1c498d498029f1e218356ae3253fade86
Shixuan Huang <93960750+rockNhu@users.noreply.github.com>
2023-05-23 20:57:00 +0800

    Update split_contig.py

--- make_db/split_contig.py
+++ make_db/split_contig.py
@@ -3,7 +3,7 @@ import re
 import argparse
 import subprocess
 import os
-
+# The arguments parser setting
 parser = argparse.ArgumentParser(description='manual to this script')
 parser.add_argument('--contigs', type=str, default='contigs.fa')
 parser.add_argument('--len', type=int, default=5000)
@@ -11,15 +11,16 @@ args = parser.parse_args()
 
 
 def check_folder(file_name):
+    '''If folder is existed, clean it, else make new folder'''
     if not os.path.exists(file_name):
-        _ = os.makedirs(file_name)
+        os.makedirs(file_name)
     else:
         print("folder {0} exist... cleaning dictionary".format(file_name))
         if os.listdir(file_name):
             try:
-                _ = subprocess.check_call(
+                subprocess.check_call(
                     "rm -rf {0}".format(file_name), shell=True)
-                _ = os.makedirs(file_name)
+                os.makedirs(file_name)
                 print("Dictionary cleaned")
             except Exception:
                 print("Cannot clean your folder... permission denied")
@@ -27,25 +28,29 @@ def check_folder(file_name):
 
 
 def special_match(strg, search=re.compile(r'[^ACGT]').search):
+    '''Check the sequence is not empty.'''
     return not bool(search(strg))
 
-
+# Env create
+check_folder("Split_files")
+# Some container and counter
 file_id = 0
 records = []
 cnt = 0
+# Using Biopython to parse and output.
 for record in SeqIO.parse(args.contigs, 'fasta'):
+    # Each FASTA take 1000 contigs.
     if cnt != 0 and cnt % 1000 == 0:
-        SeqIO.write(
-            records, f"Split_files/contig_{str(file_id)}.fasta", "fasta")
+        SeqIO.write(records, f"Split_files/contig_{file_id}.fasta", "fasta")
         records = []
         file_id += 1
         cnt = 0
     seq = str(record.seq)
     seq = seq.upper()
+    # Check sequence is long enough.
     if special_match(seq) and len(record.seq) > args.len:
         records.append(record)
         cnt += 1
 
-check_folder("Split_files")
-SeqIO.write(records, f"Split_files/contig_{str(file_id)}.fasta", "fasta")
-file_id += 1
+# Remained collection
+SeqIO.write(records, f"Split_files/contig_{file_id}.fasta", "fasta")
